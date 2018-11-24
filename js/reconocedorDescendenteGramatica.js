cdd2e2e65a3299d711a2a787772c60602ecb7679
unknown <01062-10@miremington.local>
2018-11-24 11:58:32 -0500

    add recognizer grammar logic

--- js/reconocedorDescendenteGramatica.js
+++ js/reconocedorDescendenteGramatica.js
@@ -7,7 +7,8 @@
 	Estudiantes Uniremington del programa ingenieria de sistemas del 7to semestre
 */
 function processGrammarOnDescendingRecognizer(grammar){
-    var symbolStack = {
+    
+   var symbolStackValues = {
         Empty: "",
         A: "i",
         I: "i",
@@ -20,16 +21,87 @@ function processGrammarOnDescendingRecognizer(grammar){
         Equals: "=",//Desapile y avance
         ClosedParenthesis: ")"//Desapile y avance
     };
-    grammar.map((data) => {
-        switch(data) {
-            case x:
+   // var a = isIdentifier("harold");
+    var symbolStack = ["Empty","A"];
+    //["Empty","A", "D", "=", "I"]
+    for (var i = 0; i < grammar.length; i++) {
+        var stackTop = symbolStack[symbolStack.length - 1];
+        var token = grammar[i];
+        switch(stackTop) {
+            case "Empty":
+                console.log("Borrame")
+                break;
+            case "A":
+                if(isIdentifier(token)){
+                    symbolStack.push("D", "=", "I");
+                    i--;
+                }
+                break;
+            case "I":
+            if(isIdentifier(token)){
+                symbolStack.pop();
+            }
+                break;
+            case "D":
+                if(isIdentifier(token)){
+                    symbolStack.push("L", "T");
+                    i--;
+                }
+                if(token == "("){
+                    symbolStack.push("L", "T");
+                    i--;
+                }
+                if(token == "-"){
+                    symbolStack.push("L", "T");
+                }
+                break;
+            case "T":
+            if(isIdentifier(token) || token === "("){
+                symbolStack.push("S", "F");
+                i--;
+            }
+                break;
+            case "L":
                 code block
                 break;
-            case y:
-                code block
+            case "F":
+            if(isIdentifier(token) || token === "("){
+                symbolStack.push("P");
+                i--;
+            }
                 break;
+            /*case "S":
+                code block
+                break;*/
+            case "P":
+            if(isIdentifier(token)){
+                symbolStack.pop();
+            }
+            if(token == "("){
+                symbolStack.push(")", "D");
+            }
+                break;
+            case "=":
+                if(token === "="){
+                    symbolStack.pop();
+                }
+                break;
+            /*case "ClosedParenthesis":
+                code block
+                break;*/
             default:
-                code block
-        }symbolStack
-	});
+                console.log("Error")
+        }
+        console.log(symbolStack);
+        console.log(grammar);
+	}
 }
+
+function isIdentifier(value){
+    var operators = ["=", "+", "-", "*", "/", "(", ")", ";"];
+    for (var i = 0; i < operators.length; i++) {
+        return (value != operators[i])
+    }
+}
+
+//console.log("Esta bueno: "+ (token == "2"))
